80138bddefa0c322cf66d74d09cc724bba8f86c0
handeanup <handeanup@gmail.com>
2019-09-06 19:49:07 +0530

    Changed setup and teardown method to class level in TestBakery class

--- bakery/tests/bakery_solution_test.py
+++ bakery/tests/bakery_solution_test.py
@@ -3,21 +3,23 @@ from src.bakery_solution import Bakery
 
 class TestBakery(object):
     
-    def setup_method(self):
-        self.bakery = Bakery()
+    @classmethod
+    def setup_class(cls):
+        cls.bakery = Bakery()
 
     def test_check_bakery_object(self):
-        assert isinstance(self.bakery, Bakery), 'Given object is not instance of Bakery class.'
+        assert isinstance(TestBakery.bakery, Bakery), 'Given object is not instance of Bakery class.'
 
     @pytest.mark.parametrize("code,pack,price,expected",[('VS5',2,6.99,1),('VS5',3,8.99,1),])
     def test_add_bakery_item_pack(self,code,pack,price,expected):
-        observed = self.bakery.add_bakery_item_pack(code,pack,price)
+        observed = TestBakery.bakery.add_bakery_item_pack(code,pack,price)
         assert expected == observed, 'Failed to add item in bakery_item_pack list.'
     
-    @pytest.mark.parametrize("name,code,expected",[('Vegemite Scroll','VS5',1),('Blueberry Muffin','MB11',1),])
+    @pytest.mark.parametrize("name,code,expected",[('Vegemite Scroll','VS5',1),('Blueberry Muffin','MB11',2),])
     def test_add_bakery_item(self,name,code,expected):
-        observed = self.bakery.add_bakery_item(name,code)
+        observed = TestBakery.bakery.add_bakery_item(name,code)
         assert expected == observed, 'Failed to add item in bakery_items list.'
 
-    def teardown_method(self):
-        del self.bakery
+    @classmethod
+    def teardown_class(cls):
+        del TestBakery.bakery
